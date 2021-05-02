21c04fd296df0e555d37d0cf2774ed3742e7cbf2
tiagobarros01 <tbarros1337@hotmail.com>
2021-05-02 15:39:16 -0300

    refactor: TrackBox is no longer a component

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import TrackBox from '../components/TrackBox';
+import { TrackingContext } from '../contexts/TrackingContext';
+import history from '../services/history';
 import GlobalStyle from '../styles/global';
+import { Input, Title, TrackBoxContainer } from '../styles/pages/Homepage';
 import dark from '../styles/themes/dark';
 
 export default function Homepage() {
+  const {
+    code, setCode, getTrackingData,
+  } = useContext(TrackingContext);
+
+  const handleClick = () => history.push('/tracks');
+
+  const handleSigned = () => {
+    getTrackingData();
+    handleClick();
+    console.log(code);
+  };
+
   return (
     <ThemeProvider theme={dark}>
       <Header />
-      <TrackBox />
+      <TrackBoxContainer>
+        <Title>
+          <h1>
+            Rastreio
+            <span>.</span>
+            fast
+          </h1>
+        </Title>
+        <div>
+          <Input
+            maxLength="15"
+            type="text"
+            value={code.toUpperCase()}
+            onChange={(event) => setCode(event.target.value)}
+            required
+            autoComplete="on"
+          />
+          <button
+            type="button"
+            onClick={handleSigned}
+          >
+            Track order
+          </button>
+        </div>
+      </TrackBoxContainer>
       <Footer />
       <GlobalStyle />
     </ThemeProvider>
