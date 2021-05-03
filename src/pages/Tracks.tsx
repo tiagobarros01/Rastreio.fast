0aca4b89055e76a227446f94218f54eeb6ba9432
tiagobarros01 <tbarros1337@hotmail.com>
2021-05-03 18:30:11 -0300

    chore: packageHistory isn't longer a component

--- src/pages/Tracks.tsx
+++ src/pages/Tracks.tsx
@@ -1,15 +1,76 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import React, { useContext } from 'react';
 
-import { Header } from '../components/Header';
-import PackageHistory from '../components/PackageHistory/index';
-import dark from '../styles/themes/dark';
+import { TrackingContext } from '../contexts/TrackingContext';
+import {
+  InfoContainer,
+  LineHistory,
+  LineHistoryContainer,
+  PackageContainer,
+  PackagesHistory,
+  PointContainer,
+} from '../styles/pages/Tracks';
 
 export default function Tracks() {
+  const {
+    code, getTrackingData, dateHour, city, uf, description,
+  } = useContext(
+    TrackingContext,
+  );
+
+  const handleSigned = () => {
+    getTrackingData();
+    console.log(code);
+  };
+
   return (
-    <ThemeProvider theme={dark}>
-      <Header />
-      <PackageHistory />
-    </ThemeProvider>
+    <>
+      <PackageContainer>
+        <PackagesHistory>
+          <h1>{code}</h1>
+          <LineHistoryContainer>
+            <PointContainer>
+              <div>.</div>
+              <div>.</div>
+              <div>.</div>
+              <div>.</div>
+              <div>.</div>
+            </PointContainer>
+            <LineHistory />
+            <InfoContainer>
+              <div>
+                <p>
+                  <strong>City: </strong>
+                  {city}
+                  <br />
+                  <strong>State: </strong>
+                  {uf}
+                  <br />
+                  <strong>Date-hour: </strong>
+                  {dateHour}
+                  <br />
+                  <strong>Description: </strong>
+                  {description}
+                </p>
+              </div>
+              <div>
+                <p>a</p>
+              </div>
+              <div>
+                <p>a</p>
+              </div>
+              <div>
+                <p>a</p>
+              </div>
+              <div>
+                <p>a</p>
+              </div>
+            </InfoContainer>
+          </LineHistoryContainer>
+        </PackagesHistory>
+      </PackageContainer>
+      <button type="button" onClick={handleSigned}>
+        CLick me
+      </button>
+    </>
   );
 }
--- src/styles/pages/Tracks.ts
+++ src/styles/pages/Tracks.ts
@@ -1,1 +1,83 @@
-export default {};
+import styled from 'styled-components';
+
+export const PackageContainer = styled.div`
+  width: 100%;
+  margin-top: 6rem;
+
+`;
+
+export const PackagesHistory = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+`;
+
+export const LineHistoryContainer = styled.div`
+  width: 85%;
+  height: 3rem;
+  margin-top: 3rem;
+
+  position: relative;
+`;
+
+export const LineHistory = styled.div`
+  height: 0.3rem;
+
+  background: ${(props) => props.theme.colors.yellow};
+`;
+
+export const PointContainer = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  position: absolute;
+  top: -0.5rem;
+
+  div {
+    width: 1.2rem;
+    height: 1.2rem;
+    background: ${(props) => props.theme.colors.yellow};
+
+    color: ${(props) => props.theme.colors.yellow};
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border: 3px solid ${(props) => props.theme.colors.text};
+
+    border-radius: 50%;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  margin-top: 1rem;
+
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  div {
+    background: ${(props) => props.theme.colors.primary};
+    filter: brightness(1.2);
+    
+    padding: 1rem;
+    border-radius: 10px;
+  }
+
+  p {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+
+  }
+`;
