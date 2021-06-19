ac74e407a5405b8ec679751681ae79e1a31109ab
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-19 02:48:49 -0300

    style: Changes color of nav text by useLocation

--- src/components/Navigation/Navigation.web.tsx
+++ src/components/Navigation/Navigation.web.tsx
@@ -1,17 +1,40 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { useRoutes } from '../../services/useRoutes';
-import { List } from './style';
+import { Container, List } from './style';
 
 export function NavigationWeb(): JSX.Element {
+  const { pathname } = useLocation();
+
   return (
-    <List>
-      <li>Integrations</li>
-      <li onClick={() => useRoutes('/searchcep')}>Search CEP</li>
-      <li onClick={() => useRoutes('/login')}>Login</li>
-      <li onClick={() => useRoutes('/signin')}>Sign In ⚡</li>
-    </List>
+    <Container>
+      <List
+        location={pathname === '' && true}
+        onClick={() => useRoutes('/searchcep')}
+      >
+        Integrations
+      </List>
+      <List
+        location={pathname === '/searchcep' && true}
+        onClick={() => useRoutes('/searchcep')}
+      >
+        Search CEP
+      </List>
+      <List
+        location={pathname === '/login' && true}
+        onClick={() => useRoutes('/login')}
+      >
+        Login
+      </List>
+      <List
+        location={pathname === '/signin' && true}
+        onClick={() => useRoutes('/signin')}
+      >
+        Sign In ⚡
+      </List>
+    </Container>
   );
 }
--- src/components/Navigation/style.ts
+++ src/components/Navigation/style.ts
@@ -1,26 +1,22 @@
 import styled from 'styled-components';
 
-export const List = styled.ul`
+export const Container = styled.ul`
   display: flex;
   align-items: center;
   justify-content: center;
+`;
 
-  li {
-    padding: 1rem;
-    color: ${({ theme }) => theme.colors.text};
+export const List = styled.li<{ location: boolean }>`
+  padding: 1rem;
+    color: ${({ location, theme }) => (location === true ? theme.colors.yellow : theme.colors.text)};
     font-size: 1.1rem;
     font-weight: bold;
 
     transition: filter transform 200ms;
     cursor: pointer;
 
-    :last-child {
-      color: ${({ theme }) => theme.colors.yellow};
-    }
-
     :hover {
       filter: brightness(0.8);
       transform: translateY(-2px);
     }
-  }
 `;
