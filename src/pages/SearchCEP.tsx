7f78c098aa9b5436f34b044f8a9256b33967db2d
tiagobarros01 <tbarros1337@hotmail.com>
2021-06-28 17:40:38 -0300

    feat: Creates a toast if an error CEP

--- src/pages/SearchCEP.tsx
+++ src/pages/SearchCEP.tsx
@@ -5,8 +5,10 @@ import { DataCEPProps } from '../@types/DataCEPProps';
 import { DataCEP } from '../components/DataCEP';
 import { CEPInput } from '../components/Input';
 import { Loader } from '../components/Loader';
+import { useTheme } from '../hooks/useTheme';
 import { cepAPI } from '../services/api';
 import { Container, Title, CEPContainer } from '../styles/pages/SearchCEP';
+import { useToast } from '../utils/useToast';
 
 type DataProps = {
   bairro: string;
@@ -16,20 +18,36 @@ type DataProps = {
   localidade: string;
   logradouro: string;
   uf: string;
+
+  erro?: boolean;
 }
 
 export default function SearchCEP(): JSX.Element {
+  const { theme } = useTheme();
+
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [CEPData, setCEPData] = useState<DataCEPProps | null>(null);
   const [CEPCode, setCEPCode] = useState<string>('');
-
-  console.log(isLoading);
 
   async function handleSearchCEP(cep: string): Promise<void> {
     setIsLoading(true);
 
     try {
       const { data } = await cepAPI.get<DataProps>(`${cep}/json/`);
+
+      console.log(data.erro);
+
+      if (data.erro) {
+        useToast({
+          message: 'Invalid CEP',
+          type: 'error',
+          background: theme.title === 'light' ? '#353230' : '#ddd',
+          color: theme.title === 'light' ? '#eee' : '#222',
+        });
+        setIsLoading(false);
+
+        return;
+      }
 
       setCEPData({
         cep: data.cep,
