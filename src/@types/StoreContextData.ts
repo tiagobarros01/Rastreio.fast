4e6cdfbde60ccec59d3e45020980f7c79db3cfa4
tiagobarros01 <tbarros1337@hotmail.com>
2021-07-28 20:39:47 -0300

    chore: Removes unnecessary files

--- src/@types/StoreContextData.ts
+++
@@ -1,6 +0,0 @@
-import { User } from './User';
-
-export type StoreContextData = {
-  user: User | null;
-  setUser: React.Dispatch<React.SetStateAction<User | null>>;
-};
--- src/@types/User.ts
+++
@@ -1,8 +0,0 @@
-export type User = {
-  userName: string | null;
-  email: string | null;
-  name: string | null;
-  lastName: string | null;
-  password: string | null;
-  confirmPassword: string | null;
-};
