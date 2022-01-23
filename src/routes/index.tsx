8b4a961e14b454cac97ef3244d4923eda7e58049
Tiago Barros <tbarros1337@hotmail.com>
2022-01-23 15:32:52 -0300

    feat: Creates a private Route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,10 +7,19 @@ import { Integrations } from 'src/pages/Integrations';
 import { SearchCEP } from 'src/pages/SearchCEP';
 import { Tracks } from 'src/pages/Tracks';
 
+import { PrivateRoute } from './Route.private';
+
 export const MainRoutes = (): JSX.Element => (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/tracks" element={<Tracks />} />
+    <Route
+      path="/tracks"
+      element={(
+        <PrivateRoute redirectTo="/">
+          <Tracks />
+        </PrivateRoute>
+    )}
+    />
     <Route path="/collections" element={<Collections />} />
     <Route path="/searchcep" element={<SearchCEP />} />
     <Route path="/integrations" element={<Integrations />} />
